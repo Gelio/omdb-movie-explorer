cf2b5d9109ff2f4088afdc3b08b86e6e77a6f246
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-06-14 20:40:34 +0200

    Install Material UI and remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Card, CardContent, Typography } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width={300} height={200} margin={2}>
+      <Card>
+        <CardContent>
+          <Typography>Hey</Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
